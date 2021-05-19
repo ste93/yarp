daba055003bbebcd85e3059f0f6a9c8540a3c268
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2021-05-19 17:30:33 +0200

    updated license

--- src/carriers/websocket/WebSocket/WebSocket.h
+++ src/carriers/websocket/WebSocket/WebSocket.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
 // WebSocket, v1.00 2012-09-13
 //
 // Description: WebSocket RFC6544 codec, written in C++.
